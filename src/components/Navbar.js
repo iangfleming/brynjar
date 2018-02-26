de521d5802e104845be77592d4c40d841eb30c68
Ian Fleming <fyedka2@gmail.com>
2018-02-26 17:22:43 -0600

    Nav underline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import glamorous from "glamorous";
 
 class Navbar extends Component {
+  state = {
+    activeLinkRect: {}
+  };
+  componentDidMount() {
+    const workLinkNode = ReactDOM.findDOMNode(this.workLinkRef);
+    const workLinkRect = workLinkNode.getBoundingClientRect();
+    this.setState({ activeLinkRect: workLinkRect });
+  }
+  getLinkPosition = evt => {
+    const rect = evt.target.getBoundingClientRect();
+    this.setState({ activeLinkRect: rect });
+    console.log(this.state.activeLinkRect);
+  };
   render() {
     const Header = glamorous.header({
       height: "3rem",
       display: "flex",
       alignItems: "center"
-    })
+    });
     const HeaderLink = glamorous.a({
       margin: "0 1rem",
       cursor: "pointer"
-    })
+    });
     const HeaderLinkUnderline = glamorous.div({
-      height: "2px",
-      width: "25px",
       background: "black",
-      position: "fixed"
-    })
+      position: "fixed",
+      height: "2px",
+      transition: "all 175ms cubic-bezier(0.5, 0, 0.1, 1)",
+      width: this.state.activeLinkRect.width,
+      left: this.state.activeLinkRect.left,
+      top: this.state.activeLinkRect.top + this.state.activeLinkRect.height
+    });
     return (
       <Header>
-        <HeaderLink>Work</HeaderLink>
-        <HeaderLink>Life</HeaderLink>
+        <HeaderLink
+          onClick={evt => this.getLinkPosition(evt)}
+          ref={node => {
+            this.workLinkRef = node;
+          }}
+        >
+          Work
+        </HeaderLink>
+        <HeaderLink
+          onClick={evt => this.getLinkPosition(evt)}
+          ref={node => {
+            this.lifeLinkRef = node;
+          }}
+        >
+          Life
+        </HeaderLink>
         <HeaderLinkUnderline />
       </Header>
-    )
+    );
   }
 }
 
