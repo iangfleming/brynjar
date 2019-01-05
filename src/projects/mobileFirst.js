179b3d0a55c1f391dbdb531b29619f3e2bcff154
Ian Fleming <fyedka2@gmail.com>
2019-01-05 14:40:32 -0600

    MF fixes

--- src/projects/mobileFirst.js
+++ src/projects/mobileFirst.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import glamorous from "glamorous";
-import { Link } from "react-router-dom";
 import Sizes from "../vars/Sizes";
 import ProjectBackBtn from "../components/ProjectBackBtn";
 import {
@@ -9,7 +8,6 @@ import {
   HeroDesc,
   Content,
   SectionTitle,
-  BlockLink,
   FullWidth
 } from "../components/ProjectLayout";
 import Colors from "../vars/Colors";
@@ -18,7 +16,7 @@ class mobileFirst extends Component {
   render() {
     return (
       <div>
-        <ProjectBackBtn white/>
+        <ProjectBackBtn white />
         <Hero bg={Colors.purple2}>
           <glamorous.Div maxWidth={Sizes.maxWidth} margin="0 auto">
             <HeroTitle white>IBM MobileFirst</HeroTitle>
@@ -29,9 +27,7 @@ class mobileFirst extends Component {
           </glamorous.Div>
         </Hero>
         <Content>
-          <SectionTitle>
-            The story of a failed project
-          </SectionTitle>
+          <SectionTitle>The story of a failed project</SectionTitle>
           <p>
             Sometimes preventing the wrong product from being made is just as
             important as building a product. Although it's weird to have nothing
@@ -40,42 +36,44 @@ class mobileFirst extends Component {
           <p>
             My first team after joining IBM Design was the MobileFirst product
             group. At the time the product managers were debating building a new
-            analytics tool for mobile developers. We already had an analytics tool, but it was basic with limited insights and capabilities.
+            analytics tool for mobile developers. We already had an analytics
+            tool, but it was basic with limited insights and capabilities.
           </p>
           <p>
-            A teammate and I researched the mobile
-            analytics space and learned what our users would need. We interviewed developers, read everything we could about the
-            domain, and dug deep into all our competitor's products.
+            A teammate and I researched the mobile analytics space and learned
+            what our users would need. We interviewed developers, read
+            everything we could about the domain, and dug deep into all our
+            competitor's products.
           </p>
-          <p>
-                                                                                                                                                                                                                                                                                                                                       
-          </p>
-   
-          <FullWidth bg={Colors.purple3} css={{padding: "2rem 0", textAlign: "center"}}>
-            <Content>
-              <glamorous.P fontSize={Sizes.head} fontFamily="Oswald">
-                We realized that the needs of our users were already being met by
-                other IBM products!
-              </glamorous.P>
-            </Content>
-          </FullWidth>
+          <p />
+          <glamorous.P fontSize={Sizes.head} fontFamily="Oswald" padding="2rem 0">
+            We realized that the needs of our users were already being met by
+            other IBM products!
+          </glamorous.P>
           <p>
             As crazy as it may seem, in a company as big as IBM there are so
             many products that it's really easy for them to overlap.
           </p>
-          <SectionTitle>Result</SectionTitle>
           <p>
-            After this realization, we designed a plan for our existing analytics
-            tool to work with, instead of against, the other IBM analytics
-            products.
+            After this realization, we designed a plan for our existing
+            analytics tool to work with, instead of against, the other IBM
+            analytics products.
           </p>
-          <p>
-            Although we didn't end up building
-            anything, I would argue that the research and synthesis we did
-            was far more valuable than just building another app. We saved the
-            company time and money and most importantly helped product teams,
-            that weren't talking to each other, connect.
-          </p>
+          <FullWidth
+            bg={Colors.purple2}
+            css={{ padding: "2rem 0", marginTop: "4rem" }}
+          >
+            <Content>
+              <SectionTitle css={{ color: "white" }}>Result</SectionTitle>
+              <glamorous.P css={{ color: "white" }}>
+                Although we didn't end up building anything, I would argue that
+                the research and synthesis we did was far more valuable than
+                just building another app. We saved the company time and money
+                and most importantly helped product teams, that weren't talking
+                to each other, connect.
+              </glamorous.P>
+            </Content>
+          </FullWidth>
         </Content>
       </div>
     );
