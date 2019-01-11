72f27bd4219919eaec85b217a63e91d45264ccbf
Ian Fleming <fyedka2@gmail.com>
2019-01-11 15:58:26 -0600

    pano layout and captions

--- src/images/pano/index.js
+++ src/images/pano/index.js
@@ -1,8 +1,45 @@
-export {default as achillLake} from "./achill-lake.jpg";
-export {default as achill} from "./achill.jpg";
-export {default as ericLakeTent} from "./eric-lake-tent.jpg";
-export {default as ericLake} from "./eric-lake.jpg";
-export {default as hawaiiBay} from "./hawaii-bay.jpg";
-export {default as hawaiiOpenValley} from "./hawaii-open-valley.jpg";
-export {default as hawaiiValleyTree} from "./hawaii-valley-tree.jpg";
-export {default as hawaiiValley} from "./hawaii-valley.jpg";
+import achillLake from "./achill-lake.jpg";
+import achill from "./achill.jpg";
+import ericLakeTent from "./eric-lake-tent.jpg";
+import ericLake from "./eric-lake.jpg";
+import hawaiiBay from "./hawaii-bay.jpg";
+import hawaiiOpenValley from "./hawaii-open-valley.jpg";
+import hawaiiValleyTree from "./hawaii-valley-tree.jpg";
+import hawaiiValley from "./hawaii-valley.jpg";
+
+const images = {
+  hawaiiBay: {
+    image: hawaiiBay,
+    caption: "Makapuʻu Lookout, Hawaii"
+  },
+  hawaiiOpenValley: {
+    image: hawaiiOpenValley,
+    caption: "Valley near Makapuʻu Lookout, Hawaii"
+  },
+  hawaiiValleyTree: {
+    image: hawaiiValleyTree,
+    caption: "Valley overlook in Kualoa Ranch, Hawaii"
+  },
+  hawaiiValley: {
+    image: hawaiiValley,
+    caption: "Another valley overlook in Kualoa Ranch, Hawaii"
+  },
+  achillLake: {
+    image: achillLake,
+    caption: "Lough Acorrymore on the island of Achill, Ireland"
+  },
+  achill: {
+    image: achill,
+    caption: "Blooming heather in Connemara National Park, Ireland"
+  },
+  ericLakeTent: {
+    image: ericLakeTent,
+    caption: "Ideal camp spot at the Copper Lakes, Absaroka-Beartooth Wilderness, WY"
+  },
+  ericLake: {
+    image: ericLake,
+    caption:
+      "Eric fly fishing in the Copper Lakes, Absaroka-Beartooth Wilderness, WY"
+  }
+};
+export default images;
--- src/memories/pano.js
+++ src/memories/pano.js
@@ -3,16 +3,68 @@ import glamorous from "glamorous";
 import Sizes from "../vars/Sizes";
 import MediaQueries from "../vars/MediaQueries";
 import Colors from "../vars/Colors";
-import * as images from "../images/pano/";
+import images from "../images/pano/";
+const Plaque = glamorous.div({
+  height: "80vh",
+  display: "flex",
+  flexFlow: "row wrap",
+  justifyContent: "center",
+  alignItems: "center"
+});
+const Title = glamorous.h2({
+  marginRight: "5rem",
+  letterSpacing: "4px",
+});
+const Desc = glamorous.p({
+  width: "20rem",
+});
+const Figure = glamorous.figure({
+  height: "100vh",
+  background: "black",
+  margin: 0,
+  display: "flex",
+  alignItems: "center",
+  textAlign: "right",
+});
 const StyledImg = glamorous.img({
-  width: "10rem"
-})
-const Pano = (props) => {
-  const imagesArray = Object.values(images)
+  width: "100vw"
+});
+const FigCaption = glamorous.figcaption({
+  fontSize: Sizes.sub,
+  fontStyle: "italic",
+  color: "white",
+  paddingTop: "1rem",
+  paddingRight: "1rem",
+});
+const Pano = props => {
+  const imagesArray = Object.values(images);
   return (
-    imagesArray.map(image => (
-      <StyledImg src={image} />
-    ))
+    <React.Fragment>
+      <Plaque>
+        <Title>pan•o•ram•a</Title>
+        <Desc>
+          <p>
+            Over the past five years I've made a point of taking panoramic
+            photos whenever I find a particularly captivating viewpoint.
+          </p>
+          <p>
+            There is something special about a panorama. When I look at these I
+            am transported back to that time and place.
+          </p>
+          <p>
+            It feels so vivid.
+          </p>
+        </Desc>
+      </Plaque>
+      {imagesArray.map(image => (
+        <Figure>
+          <div>
+            <StyledImg src={image.image} />
+            <FigCaption>{image.caption}</FigCaption>
+          </div>
+        </Figure>
+      ))}
+    </React.Fragment>
   );
 };
 export default Pano;
