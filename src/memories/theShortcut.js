7ac0c42f019e31d0b0eadeaaaeb4ddc2fbd543ac
Ian Fleming <fyedka2@gmail.com>
2019-03-14 11:03:55 -0500

    grammer

--- src/memories/theShortcut.js
+++ src/memories/theShortcut.js
@@ -4,7 +4,7 @@ import Sizes from "../vars/Sizes";
 import MediaQueries from "../vars/MediaQueries";
 import Colors from "../vars/Colors";
 import ProjectBackBtn from "../components/ProjectBackBtn";
-import p1 from "../images/theShortcut/GC032431.JPG";
+import p1 from "../images/theShortcut/GC032431.jpg";
 
 const Plaque = glamorous.div({
   display: "flex",
@@ -53,17 +53,17 @@ const TheShortcut = props => {
         <StyledImg href={p1} />
         <Desc>
           <p>
-            I've always had this curiosity. As a kid it was satisfied by
+            I've always had this curiosity. As a kid, it was satisfied by
             wandering farther and farther into the bushes behind the house. As I
             grew up so did the scope of these explorations. It can be as simple
             as discovering what is at the end of that "dead-end" street on the
             next block or, as in this particular case, much more complex and
-            time consuming.
+            time-consuming.
           </p>
           <p>
-            Texas is defined by it's privately held land. In a state know for
-            it's vast empty spaces the average citizen is contained to thin
-            strips of public land, roads and parks, sandwiched in between huge
+            Texas is defined by it's privately held land. In a state known for
+            its vast empty spaces, the average citizen is contained to thin
+            strips of public land, roads, and parks, sandwiched in between huge
             tracts with "no trespassing" emblazoned on their gates.
           </p>
           <p>
@@ -72,7 +72,7 @@ const TheShortcut = props => {
             land.
           </p>
           <p>
-            Politics aside, this can be incredibly frustating for someone like
+            Politics aside, this can be incredibly frustrating for someone like
             myself who is driven by their curiosity to explore. However, as with
             many types of adversity, these challenges have created some very
             clever solutions. They force us to look for hidden paths and disused
@@ -96,7 +96,7 @@ const TheShortcut = props => {
             reasonably safe, you typically have to take the route shown above.
             This is a beautiful ride but gets to be a pretty long day. If you
             want to shorten the route you'd have to ride east on Hamilton Pool
-            Rd. which any self respecting Austin cyclist will tell you is a
+            Rd. which any self-respecting Austin cyclist will tell you is a
             terribly dangerous idea.
           </p>
           <p>
@@ -116,17 +116,18 @@ const TheShortcut = props => {
             src="https://www.strava.com/activities/1582766921/embed/ff31689487798fb9328c55597e7c37ddfe7da3c8"
           />
           <p>
-            And well, it worked! As you can see the route was signicifcantly
-            shorter with much less climbing. But most importantly it satisified
+            And well, it worked! As you can see the route was significantly
+            shorter with much less climbing. But most importantly it satisfied
             that love of getting lost, seeing something you haven't seen before,
             and finding your way out.
           </p>
           <p>
-            Researching and riding routes like this has become one of my
+            Researching and riding routes like this have become one of my
             favorite hobbies. If you have any questions about this one or any
             other dubious shortcuts give me a shout!
           </p>
         </Desc>
+        <StyledImg src={p1} />
       </Plaque>
     </React.Fragment>
   );
